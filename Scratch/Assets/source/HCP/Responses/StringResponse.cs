23d82b6c3e359032c561c1f4d72ae534e0dc6daa
Colin Nickerson <colinn@gopokyo.com>
2016-12-04 11:03:08 -0400

    init

---
+++ Scratch/Assets/source/HCP/Responses/StringResponse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HCP;
+
+namespace HCP.Responses
+{
+    public class StringResponse : JobResponse
+    {
+        public StringResponse() : base("")
+        {
+        }
+
+        public StringResponse(string value) : base(value)
+        {
+        }
+    }
+}
